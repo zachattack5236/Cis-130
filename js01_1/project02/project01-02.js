7248d90b8d332ee87c671b8a0971cfdff89d3c8a
zachattack5236 <zachattack5236@gmail.com>
2025-09-22 18:21:30 -0400

    should be the complete projects of chapter 1   1-4

---
+++ js01_1/project02/project01-02.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Zachary Carpenter
+    Date:   9/22/2025
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+
+let service1Speed = "0 Mbps";
+let service2Speed = "100 Mbps";
+let service3Speed = "500 Mbps";
+let service4Speed = "1 Gig";
